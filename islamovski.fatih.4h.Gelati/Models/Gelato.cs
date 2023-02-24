e1be98651ab93992dec39a2bd53eae03b0742f1a
fatihisla <116791165+fatihisla@users.noreply.github.com>
2023-02-24 09:50:26 +0100

    prima versione

---
+++ islamovski.fatih.4h.Gelati/Models/Gelato.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace islamovski.fatih._4h.Gelati.Models
+{
+    public class Gelato
+    {
+        public string Tipo { get; set; }
+        public string Size { get; set; }
+        public string Gusto { get; set; }
+        public Gelato() { }
+
+        public Gelato(string riga)
+        {
+            string[] colonne = riga.Split(';');
+            Tipo = colonne[0];
+            Size = colonne[1];
+            Gusto = colonne[2];
+        }
+
+    }
+
+    public class Gelati : List<Gelato>
+    {
+       
+
+        public Gelati(string nomefile)
+        {
+            try
+            {
+
+                StreamReader fin = new StreamReader(nomefile);
+                fin.ReadLine();
+                while (!fin.EndOfStream)
+                {
+                    string riga = fin.ReadLine();
+                    Gelato g = new Gelato(riga);
+                    Add(g);
+                }
+                fin.Close();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
